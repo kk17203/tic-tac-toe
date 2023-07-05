cd1cd3109ecbcff16786dda8bd2f766b13d4375b
kk17203 <kk17203@gmail.com>
2023-07-05 16:42:00 -0500

    Finish game functions. Add style to form

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
+function openForm() {
+    document.querySelector(".form-popup").style.display = "flex";
+};
+function closeForm() {
+    document.querySelector(".form-popup").style.display = "none";
+};
+
+
+
+
+//displayController handles the messages sent to 'div' class message in HTML
+const displayController = (() => {
+    const renderMessage = (message) => {
+        document.querySelector(".message").textContent = message;
+    }
+    return {
+        renderMessage,
+    }
+})();
+
 const Gameboard = (() => {
     // Array to store gameboard
     let gameboard = ["", "", "", "", "", "", "", "", ""]
 
-        // render is the way to display the gameboard
+    // 'render' function renders the gameboard UI
     const render = () => {
         let boardHTML = "";
         gameboard.forEach((square, index) => {
-            // Each section of array creates a div with class 'square'. Each one gets uniqe id based on index. The contect of each array element is used for textcontect of each array (X or O).
+            // Each section of array creates a div with class 'square'. Each square gets uniqe id based on index. The context of each array element is used for textcontext of each div (X or O).
             boardHTML += `<div class="square" id="square-${index}">${square}</div>`
         })
-        //put each one of the created div's in the gamebaord (assigning the HTML not just text content). Next line needs to be included in curly brackets of 'render' for boardHTML to be accessible(because of scope)
+        //put each one of the created div's in the gamebaord (assigning the HTML not just text context). Next line needs to be included in curly brackets of 'render' for boardHTML to be accessible(because of scope)
         document.querySelector(".gameboard").innerHTML = boardHTML;
         const squares = document.querySelectorAll(".square");
         // attach an eventListener to each square
@@ -18,12 +38,17 @@ const Gameboard = (() => {
             })
     }
 
-    //update the gamebaord. handleClick is giving use the arg for this 'update' function. First arg (index), handleClick gives us the index of the square we clicked. Second arg (value), handleClick gives us the value of the current players mark. 
+    // 'update' function updates the 'gameboard' array & UI after each move
+    //update the gamebaord. handleClick is giving us the arg for this 'update' function. First arg (index), handleClick gives us the index of the square we clicked. Second arg (value), handleClick gives us the value of the current players mark. 
     const update = (index, value) => {
         // next line assigns the value given from handleClick(mark) to the gameboard index value given from handleClick. This updates the gamboard array.
         gameboard[index] = value;
         // re run render() to show updated array values in UI
         render();
+    }
+
+    const removeBoard = () => {
+        document.querySelector(".gameboard").innerHTML = "";
     }
 
     //used to access gameboard indirectly so we don't modify it 
@@ -34,10 +59,11 @@ const Gameboard = (() => {
         render,
         update,
         getGameboard,
+        removeBoard,
     }
 })();
 
-// make a Factory to create players
+// FactoryFunction to create players
 const createPlayer = (name, mark) => {
     return {
         name, 
@@ -45,6 +71,7 @@ const createPlayer = (name, mark) => {
     }
 }
 
+//'Game' module handles the logic for the game
 const Game = (() => {
     // next 3 variables just declare them. Set the contents under 'start'
     // Keep track of players
@@ -54,8 +81,9 @@ const Game = (() => {
     // want to know when game is over
     let gameOver;
 
-    //Call on createPlayer Factory 
+    // Handles game start functions 
     const start = () => {
+        // Calls on 'createPlayer' FactoryFunction to make array
         players = [
             createPlayer(document.querySelector("#player1").value, "X"),
             createPlayer(document.querySelector("#player2").value, "O"),
@@ -79,25 +107,82 @@ const Game = (() => {
         
         console.log(`Square Index: ${index}`);
         console.log(`Current Player Index: ${currentPlayerIndex}`);
+
         Gameboard.update(index, players[currentPlayerIndex].mark)
         
+        if(checkForWin(Gameboard.getGameboard())) {
+            gameOver = true;
+            displayController.renderMessage(`${players[currentPlayerIndex].name} Won!`);
+        }
+        if(checkForTie(Gameboard.getGameboard())) {
+            gameOver = true;
+            displayController.renderMessage('Tie Game!');
+        }
+
         // changes currentPlayerIndex after each turn. If cPI === 0 then it assigns new value 1. if not 0 then it assigns 0.
         currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    }
+
+    const restart = () => {
+        for (let i = 0; i < 9; i++) {
+            Gameboard.update(i, "");
+        }
+    const player1 = document.querySelector("#player1");
+    const player2 = document.querySelector("#player2");
+    player1.value = "";
+    player2.value = "";
+    players =['',''];
+    Gameboard.removeBoard();
+    currentPlayerIndex = 0;
+    displayController.renderMessage("");
     }
     
     return {
         start,
         handleClick, 
+        restart,
     }
 
 })( );
 
+function checkForTie(board) {
+    //Checks to see if EVERY cell is empty. If so returns true
+    return board.every(cell => cell !=="")
+}
+
+function checkForWin(board) {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7 ,8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    for(let i = 0; i < winningCombinations.length; i++) {
+        // Next line is short hand. It links the current winning combinations array with a, b, c.
+        // for example winningCombinations[2] a=6, b=7, c=8.
+        const [a, b, c] = winningCombinations[i];
+        //board[a] checks to make sure it atleast has a value. Then the rest checks to see if the board matches a winningCombination
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return true;
+        }
+    }
+}
 
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener('click', ()=> {
     Game.start();
+    closeForm();
 });
 const resetButton = document.querySelector("#reset-button");
 resetButton.addEventListener('click', ()=> {
-    alert('reset')
+    Game.restart();
+    openForm();
+});
+const openFormButton = document.querySelector("#open-form");
+openFormButton.addEventListener('click', ()=> {
+    openForm();
 });
